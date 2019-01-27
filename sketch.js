3cea9d5aaec623e4f22627929436a25c9eb298e2
MiguiTE <miguite19@gmail.com>
2019-01-27 22:25:30 +0100

    Finish Bubble sort
    Finished bubble sort algorithm
    with visualization

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ let generateButton;
 let sortButton;
 let sel;
 let func = "Insertion sort";
+let i = 1;
+let j = i;
+let newi = 0;
 
 function setup() {
 	createCanvas(800,800);
@@ -23,15 +26,30 @@ function setup() {
 	sel.option("Merge sort");
 	sel.option("Quick sort");
 	sel.position(110, height + 40)
-	sel.changed(() => func = sel.value())
+	sel.changed(() => {
+		func = sel.value();
+		sorted = true;
+		switch (func) {
+			case "Insertion sort":
+				i = 1;
+				j = i;
+				break;
+			case "Bubble sort":
+				i = bars.length;
+				j = 0;
+				newi = 0;
+				break;
+			case "Merge sort":
+				break;
+			case "Quick sort":
+				break;
+		}
+	});
 
 	sortButton = createButton("Sort");
 	sortButton.position(250,height + 40)
 	sortButton.mousePressed(() => sorted = false);
 }
-
-let i = 1;
-let j = i
 
 function draw() {
 	background(151)
@@ -57,6 +75,31 @@ function draw() {
 					i = 1;
 					j = i;
 				}
+				break;
+			case "Bubble sort":
+				if(j<i){
+					fill(255,255,0)
+					rect(newi*barwidth,height,barwidth,-bars[newi]);
+					if(bars[j-1]>bars[j]){
+						swap(bars,j,j-1);
+						newi=j;
+					}
+					j++;
+				}else{
+					i = newi;
+					j = 0;
+					newi = 0;
+				}
+				if(i==0){
+					sorted = true;
+					i = bars.length;
+					j = 0;
+					newi = 0;
+				}
+				break;
+			case "Merge sort":
+				break;
+			case "Quick sort":
 				break;
 		}
 	}
