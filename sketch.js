2b9eb4e270aaad9017db0554f602c91dd36e718f
MiguiTE <miguite19@gmail.com>
2019-01-27 21:40:33 +0100

    Add basic structure.
    Add buttons to generate random numbers,
    choose algorithm to sort,
    and sort.

--- sketch.js
+++ sketch.js
@@ -2,11 +2,32 @@ const total = 100;
 let bars = [];
 let barwidth = 0;
 let sorted = false;
+let generateButton;
+let sortButton;
+let sel;
+let func = "Insertion sort";
+
 function setup() {
 	createCanvas(800,800);
 	background(150);
 	barwidth = width / total;
-	generate();
+
+
+	generateButton = createButton("Generate");
+	generateButton.position(10,height + 40)
+	generateButton.mousePressed(() => generate());
+
+	sel = createSelect();
+	sel.option("Insertion sort");
+	sel.option("Bubble sort");
+	sel.option("Merge sort");
+	sel.option("Quick sort");
+	sel.position(110, height + 40)
+	sel.changed(() => func = sel.value())
+
+	sortButton = createButton("Sort");
+	sortButton.position(250,height + 40)
+	//sortButton.mousePressed();
 }
 function draw() {
 	background(151)
