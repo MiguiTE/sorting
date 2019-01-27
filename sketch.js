26372e3a2d1f48952bdfe57a3f5eb888bf3675bd
MiguiTE <miguite19@gmail.com>
2019-01-27 22:43:49 +0100

    Clear version
    Add comments.
    Change swap function.
    Arrow function for generate random numbers.

--- sketch.js
+++ sketch.js
@@ -1,31 +1,45 @@
-const total = 100;
-let bars = [];
-let barwidth = 0;
-let sorted = true;
+// Some usefull variables
+const total = 100; //total number of bins
+let bars = []; //all the bins
+let barwidth = 0; // width of the bins
+let sorted = true; //boolean to check if array is sorted. Set to true because I dont want to sort from the beggining
+// Buttons and selector
 let generateButton;
 let sortButton;
 let sel;
+
+// keep track of the algorithm to sort
 let func = "Insertion sort";
+//usefull variables for the algorithms
 let i = 1;
 let j = i;
 let newi = 0;
 
 function setup() {
+	//Start the canvas and barwidth
 	createCanvas(800,800);
 	background(150);
 	barwidth = width / total;
 
-
+	//Generate button
 	generateButton = createButton("Generate");
 	generateButton.position(10,height + 40)
-	generateButton.mousePressed(generate);
+	generateButton.mousePressed(() => {
+		bars = [];
+		for (let i = 0; i < total; i++) {
+			const element = random(height);
+			bars.push(element);
+		}
+	});
 
+	//Selector
 	sel = createSelect();
 	sel.option("Insertion sort");
 	sel.option("Bubble sort");
 	sel.option("Merge sort");
 	sel.option("Quick sort");
 	sel.position(110, height + 40)
+	//If changed select algorithm and start its usefull variables
 	sel.changed(() => {
 		func = sel.value();
 		sorted = true;
@@ -46,25 +60,30 @@ function setup() {
 		}
 	});
 
+	//Sorting button
 	sortButton = createButton("Sort");
 	sortButton.position(250,height + 40)
 	sortButton.mousePressed(() => sorted = false);
 }
 
 function draw() {
+	//draw all the bins in each loop
 	background(151)
 	for (let i = 0; i < bars.length; i++) {
 		const altura = bars[i];
 		fill(0,255,0)
 		rect(i*barwidth,height,barwidth,-altura);
 	}
+
+	//When sort button clicked sorted=false
 	if (!sorted && bars.length > 0){
 		switch (func) {
 			case "Insertion sort":
 				fill(255,255,0)
 				rect(j*barwidth,height,barwidth,-bars[j]);
 				if (j > 0 && bars[j-1] > bars[j]){
-					swap(bars, j, j-1);
+					//swap
+					bars[j], bars[j-1] = bars[j-1], bars[j]
 					j--;	
 				}else{
 					i++;
@@ -81,7 +100,8 @@ function draw() {
 					fill(255,255,0)
 					rect(newi*barwidth,height,barwidth,-bars[newi]);
 					if(bars[j-1]>bars[j]){
-						swap(bars,j,j-1);
+						//swap
+						bars[j], bars[j-1] = bars[j-1], bars[j]
 						newi=j;
 					}
 					j++;
@@ -102,18 +122,5 @@ function draw() {
 			case "Quick sort":
 				break;
 		}
-	}
-}
-function swap(array, i, j) {
-	let tmp = array[i];
-	array[i] = array[j];
-	array[j] = tmp;
-}
-
-function generate(){
-	bars = [];
-	for (let i = 0; i < total; i++) {
-		const element = random(height);
-		bars.push(element);
 	}
 }
